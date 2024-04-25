b5c9456e69e2b40c4747b241649f7b91171e14bc
Luis Machado <luismachadopnf@gmail.com>
2024-04-25 09:20:13 +0200

    Fix tailwind/daisyui config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js,ts}"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui"), { tailwindcss: {}, autoprefixer: {} }],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark", "cupcake", "dim"],
+  },
 };
