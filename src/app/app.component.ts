390bead078701dc0d24ec2a1f87ad39c34eca163
Luis Machado <luismachadopnf@gmail.com>
2024-04-29 18:26:41 +0200

    Add the theme toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { themeChange } from 'theme-change';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'my-resume';
+
+  constructor() { 
+    themeChange();
+  }
 }
